c95e8e1450d6384612eaad22596c4885e7f7e585
mad_md <mad.md@yandex.ru>
2026-01-12 10:25:27 +0300

    Исправление: замена хардкода 'Laravel' на config('app.name') в title страниц

--- resources/views/resources/stub.blade.php
+++ resources/views/resources/stub.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>{{ __('moonshine.types.values.' . $type->name) ?: $type->name }} - Laravel</title>
+        <title>{{ __('moonshine.types.values.' . $type->name) ?: $type->name }} - {{ config('app.name') }}</title>
 
         <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
