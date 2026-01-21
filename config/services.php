04d54d5e0e977c9be05138d97599dd08154f7162
madmd <madmd@yandex.ru>
2026-01-22 01:39:13 +0300

    ﻿РћР±РЅРѕРІРёС‚СЊ VK app_id РІ РЅР°СЃС‚СЂРѕР№РєР°С….

--- config/services.php
+++ config/services.php
@@ -40,7 +40,7 @@ return [
         'api_version' => env('VK_API_VERSION', '5.131'),
         'api_url' => env('VK_API_URL', 'https://api.vk.com/method'),
         'verify_ssl' => env('VK_VERIFY_SSL', true),
-        'app_id' => env('VK_APP_ID', '54418904'),
+        'app_id' => env('VK_APP_ID', '54428914'),
         'redirect_url' => env('VK_REDIRECT_URL', 'https://m-engine.ru'),
         'tunnel_url' => env('VK_TUNNEL_URL', null), // URL туннелинг сервиса (например, https://m-engine.loca.lt)
     ],
